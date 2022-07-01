18b4bcb71cbf3e174e82412e5fcc34212f95e9ab
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-07-01 16:14:09 +0600

    udpated: recursive_binary.py

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -16,6 +16,9 @@ def verify(index):
         return print("Target found")
     else:
         return print("Target not found")
+
+
+        
 list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 index=recursive_binary_search(list,12)
 index2=recursive_binary_search(list,23)
