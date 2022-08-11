82d7f39ff0fb29260b681c7b56df324b0cb6e1c4
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-08-12 00:02:10 +0600

    singlyLikedList finished

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,4 +1,5 @@
 class Node:
+
     def __init__(self, data, next_node = None):
         self.data = data
         self.next_node = next_node
@@ -6,7 +7,8 @@ class Node:
           return "<Node data: %s>" % self.data
 
 
-class LinkedList:
+class SinglyLinkedList:
+
     def __init__(self):
         self.head = None
         self.count = 0
@@ -25,6 +27,50 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self,key):
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        if index == 0:
+            return self.add(data)
+        
+        if index > 0:
+            current = self.head
+            new_node = Node(data)
+            position = index
+            
+            while position > 1:
+                current = current.next_node
+                position -=1
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new_node
+            new_node.next_node = next_node
+    def remove(self, key):
+        current = self.head
+        found = False
+        previous = None
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True 
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        return current
+
     def __repr__(self):
         nodes = []
         current = self.head
@@ -38,3 +84,5 @@ class LinkedList:
                 nodes.append("[%s]" % current.data)
             current = current.next_node
         return "-> ".join(nodes)
+
+        
